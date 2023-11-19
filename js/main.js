7d03a9a42881f7f867a06bcf94f724d6250dcd48
unknown <nikolay.amd2012@yandex.ru>
2023-11-19 21:04:54 +0300

    Commit - 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Swiper for Aboutcity Section //
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+
+// Animation for Sections //
+new WOW().init();
